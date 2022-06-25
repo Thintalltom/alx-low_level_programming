facbe56d514a4717c8c049e53f344e6d0a480be0
Thintalltom <adeyanjutomide@gmail.com>
2022-06-25 13:33:34 -0700

    new changes task 7

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,6 +8,6 @@ int main(void)
 
 	char c[20] = "2015-10-19";
 
-	fprintf(stderr, "and that piece of art is useful\" - Dora kopar %s \n",  c);
+	fprintf(stderr, "and that piece of art is useful\" - Dora kopar, %s \n",  c);
 	return (1);
 }
