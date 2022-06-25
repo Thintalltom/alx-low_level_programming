5987e18305478a18ec46ac02e02e636622c0c9fb
Thintalltom <adeyanjutomide@gmail.com>
2022-06-25 03:15:53 -0700

    task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Entry point with the word with proper grammar
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
