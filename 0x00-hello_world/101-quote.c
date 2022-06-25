a0ee4ccf6226bbbe4eedec696132905810e00cfc
Thintalltom <adeyanjutomide@gmail.com>
2022-06-25 07:26:17 -0700

    changes made to task 7

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
-#include <unistd.h>
 /**
  * main -Entry point
  * Return: Always 1 (success)
  */
 int main(void)
 {
-write(2 "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	putc("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stderr);
 return (1);
 }
