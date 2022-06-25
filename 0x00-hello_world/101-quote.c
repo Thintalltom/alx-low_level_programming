d7f7c37516cf62ca08a569e5872d3dc3bdf50c39
Thintalltom <adeyanjutomide@gmail.com>
2022-06-25 13:31:59 -0700

    task 7

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,6 +5,9 @@
  */
 int main(void)
 {
-	fprintf(stderr, "and that piece of art is useful\"- Dora Korpar, 2015-10-19");
+
+	char c[20] = "2015-10-19";
+
+	fprintf(stderr, "and that piece of art is useful\" - Dora kopar %s \n",  c);
 	return (1);
 }
