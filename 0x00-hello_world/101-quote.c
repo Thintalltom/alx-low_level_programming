2389462af6663a5a47f51f14bf5afd5b7161b755
Thintalltom <adeyanjutomide@gmail.com>
2022-06-26 06:04:13 -0700

    new

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,8 +6,6 @@
  */
 int main(void)
 {
-	char a[20] = "and that piece of art is useful";
-
-	write(2, "%s\"- Dora Korpar, 2015-10-19", a, 90);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 90);
 	return (1);
 }
