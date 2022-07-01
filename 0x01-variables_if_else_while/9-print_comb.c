6bccd69a325243cdd20e52595799eb434736ece4
Thintalltom <adeyanjutomide@gmail.com>
2022-07-01 14:26:29 -0700

    Task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - return 0
+ *
+ * Return: Always (Success)
+ */
+int main(void)
+{
+int a;
+
+for (a = 0; a < 10; a++)
+{
+putchar((a % 10) + '0');
+putchar(',');
+}
+
+putchar('\n');
+return (0);
+
+
+}
