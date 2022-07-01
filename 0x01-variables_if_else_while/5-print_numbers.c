e427057d8904d363c31aa4e3c17b5af9695584a9
Thintalltom <adeyanjutomide@gmail.com>
2022-07-01 11:08:39 -0700

    Task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+ * main - return the value 0 and also printnumber 0 to 10
+ *
+ * Return: Always (Success)
+ */
+
+int main(void)
+{
+int a;
+a = 0;
+while (a < 10)
+{
+printf("%d\n", a);
+}
+a++
+return (0);
