e69f44842ac5ef187936fba6af75476b807855ec
Thintalltom <adeyanjutomide@gmail.com>
2022-07-01 12:40:57 -0700

    Task 77

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - return o
+ *
+ * Return: Always (Success(
+ */
+
+int main(void)
+{
+char ab;
+
+for (ab = 'z'; ab >= 'a'; ab--)
+{
+putchar(ab);
+}
+putchar("/n");
+return (0);
+}
