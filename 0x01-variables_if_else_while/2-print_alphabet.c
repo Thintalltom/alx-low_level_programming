493349d148b0cf98803fb7f742443fd585640c4d
Thintalltom <adeyanjutomide@gmail.com>
2022-07-01 10:17:39 -0700

    Task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - write program in lowercase
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+
+int main(void)
+{
+	char n;
+
+	for (n = a; n = < z, n++)
+		putchar(n);
+}
