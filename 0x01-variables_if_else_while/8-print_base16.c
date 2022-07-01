b356c168974dc7259e2772ddef7c087c89a07da1
Thintalltom <adeyanjutomide@gmail.com>
2022-07-01 13:30:01 -0700

    Task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - to print hexadecimal
+ *
+ * Return: Always (Success)
+ */
+int main(void)
+{
+int a;
+char b;
+
+for (a = 0; a < 10; a++)
+{
+putchar(a);
+}
+for (b = 'a'; b < 'f'; b++)
+{
+putchar(b);
+}
+putchar('\n');
+return (0);
+}
