a150042bf6d1f831969743ed7f1591fd80beed5b
Thintalltom <adeyanjutomide@gmail.com>
2022-07-01 08:00:12 -0700

    changes made to task 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -16,6 +16,6 @@ printf("%d and is greater than 5\n", n);
 else if (n == 0)
 printf("%d and is 0\n", n);
 else if (n > 6 && n != 0)
-prinf("%dand is less than 6 and not 0\n", n);
+printf("%dand is less than 6 and not 0\n", n);
 return (0);
 }
