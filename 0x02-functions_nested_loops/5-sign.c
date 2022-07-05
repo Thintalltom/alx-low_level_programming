95b8099436d1ca77ef87dc8f9a2a900130480b4f
Thintalltom <adeyanjutomide@gmail.com>
2022-07-05 15:48:33 -0700

    Task 5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_sign - should return + - and 0
+ *@n: this is an argument
+ * Return: Always (Success)
+ */
+int print_sign(int n)
+{
+if (n > 0}
+{
+_putchar('+');
+return (1);
+}
+else if (n == 0)
+{
+_putchar('n');
+return (0);
+}
+else if (n < 0)
+{
+_putchar('-');
+return (-1);
+}
