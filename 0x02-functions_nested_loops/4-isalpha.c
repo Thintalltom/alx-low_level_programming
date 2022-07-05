7810223e230dfbc3c1b8d94e52e82cccdf0e5ef6
Thintalltom <adeyanjutomide@gmail.com>
2022-07-05 15:32:01 -0700

    Task 4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _isalpha - return 1
+ * @c: a character argument
+ * Return: return 1 and 0
+ */
+int _isalpha(int c)
+{
+return (c >= 'a' && c <= 'z');
+return (c >= 'A' && c <= 'Z');
+}
