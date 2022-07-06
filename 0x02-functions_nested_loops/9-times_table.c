75d45e5a1632134f44f409c51b93eac8f1d7b158
Thintalltom <adeyanjutomide@gmail.com>
2022-07-05 17:13:28 -0700

    Task 9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * times_tables - retrun multiplication table 9
+ *
+ * Return: Always (Success)
+ */
+void times_table(void);
+{
+int a;
+int b;
+int multi;
+for (a = 0; a <= 9; a++)
+{
+for (b = 0; b <= 12; b++)
+{
+if (a >= 0 && b >= 0)
+{
+multi = a * b;
+_putchar((multi / 10) + '0');
+_putchar((multi % 10) + '0');
+_putchar(',');
+_putchar('\t');
+}
+}
+}
