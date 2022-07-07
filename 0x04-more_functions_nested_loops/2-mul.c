60f147145f2e36ec2dbca76d13d310a6ce18c345
Thintalltom <105128485+Thintalltom@users.noreply.github.com>
2022-07-07 20:20:56 +0100

    Changes made to task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include ”main.h"
+/**
+*mul(int a; int b) - Always return 0 or 1
+*@a: the passed argument
+*@b: the passed argument
+*
+*Return: Always (Success)
+*/
+
+int mul(int a, int b)
+{
+return (a * b);
+}
