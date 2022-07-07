c5bcb5d7c66a540a2a55720424e0535a8e2a2096
Thintalltom <adeyanjutomide@gmail.com>
2022-07-07 11:15:19 -0700

    Task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _isupper(int c)- RETURN 1 IF UPPERCASE AND 0 IF ANYTHING ELSE
+ * @c: the argument
+ *
+ * Return: Always (Success)
+ */
+
+int _isupper(int c)
+{
+return ((c > = 'A' && c = < 'Z'));
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This is a new directory for a new nexted function
