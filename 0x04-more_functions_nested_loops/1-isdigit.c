4826b5fec91a16bab9db7c1fa3cb13a9613f1fc7
Thintalltom <adeyanjutomide@gmail.com>
2022-07-07 11:48:46 -0700

    Task 1

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,4 +1,13 @@
 #include "main.h"
-
 /**
- *
+ * _isdigit(int c)- Return 0 or 1
+ * @c: this is an argument
+ * *
+ * Return: Always (Success)
+ *  *
+ */
+
+int _isdigit(int c)
+{
+return ((c >= 0 && c <= 9));
+}
