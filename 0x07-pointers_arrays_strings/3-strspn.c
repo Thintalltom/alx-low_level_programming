fb04d9ced6abe6807f9f20c6a0a6128fd4e97ae6
Thintalltom <105128485+Thintalltom@users.noreply.github.com>
2022-07-18 19:21:12 +0100

    Update 3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -11,24 +11,22 @@
 
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int bytes = 0;
-	int index;
+unsigned int bytes = 0;
+int index;
 while (*s)
-	{
-		for (index = 0; accept[index]; index++)
-		{
-			if (*s == accept[index])
-			{
-				bytes++;
-				break;
-			}
+{
+for (index = 0; accept[index]; index++)
+{
+if (*s == accept[index])
+{
+bytes++;
+break;
+}
+else if (accept[index + 1] == '\0')
+return (bytes);
+}
+s++;
+}
 
-			else if (accept[index + 1] == '\0')
-				return (bytes);
-		}
-
-		s++;
-	}
-
-	return (bytes);
+return (bytes);
 }
