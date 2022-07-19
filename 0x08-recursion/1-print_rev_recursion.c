65304d1b4d1e2cb49ef06e6b11ab6ba4b5a740ca
Thintalltom <adeyanjutomide@gmail.com>
2022-07-19 14:47:42 -0700

    Task 1

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ *_print_rev_recursion - print string in reverse 
+ *
+ *@s: the argument string
+ */
+
+void _print_rev_recursion(char *s);
+{
+static int i, len, temp;
+len = strlen(s); // use strlen() to get the length of str string
+
+if (i < len/2)
+{
+// temp variable use to temporary hold the string
+temp = s[i];
+s[i] = s[len - i - 1];
+s[len - i - 1] = temp;
+i++;
+_print_rev_recursion(s); // recusively calls the revstr() function
+}  
+} 
