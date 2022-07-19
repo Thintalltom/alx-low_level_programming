e120c39957ebaae516e725dd6e5a76619fd18226
Thintalltom <adeyanjutomide@gmail.com>
2022-07-19 14:20:52 -0700

    Task 0

---
+++ 0x08_recursion.c/0-puts_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - to print a string.
+ * @s: the string that will be printed.
+ *
+ */
+
+void _puts_recursion(char *s)
+{
+_putchar(*s);
+_putchar('\n');
+}
