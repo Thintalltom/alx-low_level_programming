b49743d777c128dcfd6b79a55390a9ecdc99d5dc
Thintalltom <105128485+Thintalltom@users.noreply.github.com>
2022-07-22 15:42:06 +0100

    Update 4-isalpha.c

--- 0x09-static_libraries/4-isalpha.c
+++ 0x09-static_libraries/4-isalpha.c
@@ -1,1 +1,10 @@
-
+#include "main.h"
+/**
+ * _isalpha - return 1
+ * @c: a character argument
+ * Return: return 1 and 0
+ */
+int _isalpha(int c)
+{
+return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
+}
