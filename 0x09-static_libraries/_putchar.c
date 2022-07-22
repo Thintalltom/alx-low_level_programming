61f329756aec69736cb8a9ab9a41ba677d157040
Thintalltom <105128485+Thintalltom@users.noreply.github.com>
2022-07-22 15:56:14 +0100

    Update _putchar.c

--- 0x09-static_libraries/_putchar.c
+++ 0x09-static_libraries/_putchar.c
@@ -1,1 +1,14 @@
+#include <unistd.h>
+#include "main.h"
 
+/**
+ * _putchar - writes the character c to the stdout
+ * @c: the character to print
+ *
+ * Return: On success 1
+ * on error, -1 is required, and errno is set appropraitely.
+ */
+int_putchar(char c)
+{
+return (write(1, &c, 1));
+}
