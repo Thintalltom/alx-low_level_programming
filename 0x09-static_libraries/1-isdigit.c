89d960d7262c611ef9ce23a98637e83a99539633
Thintalltom <105128485+Thintalltom@users.noreply.github.com>
2022-07-22 15:44:18 +0100

    Update 1-isdigit.c

--- 0x09-static_libraries/1-isdigit.c
+++ 0x09-static_libraries/1-isdigit.c
@@ -1,1 +1,12 @@
+#include "main.h"
+/**
+ * _isdigit(int c)- Return 0 or 1
+ * @c: an argumnet passed
+ *
+ * Return: Always (Success)
+ */
 
+int _isdigit(int c)
+{
+return ((c >= '0' && c <= '9'));
+}
